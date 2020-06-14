1eec69d9a315a3b9d49e95101bcc1b73d7fd89ce
Alessandro Tozzi <tozzi.1751462@studenti.uniroma1.it>
2020-06-14 19:40:19 +0200

    aggiunti nome, cognome, telefono form registrazione

--- progetto_lassi/db/schema.rb
+++ progetto_lassi/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200603142913) do
+ActiveRecord::Schema.define(version: 20200614172717) do
 
   create_table "joinedtravels", force: :cascade do |t|
     t.integer "user_id"
@@ -40,10 +40,6 @@ ActiveRecord::Schema.define(version: 20200603142913) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "nome"
-    t.string "cognome"
-    t.string "telefono"
-    t.integer "rating"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -58,6 +54,10 @@ ActiveRecord::Schema.define(version: 20200603142913) do
     t.datetime "updated_at", null: false
     t.string "provider"
     t.string "uid"
+    t.string "telefono"
+    t.string "nome"
+    t.string "cognome"
+    t.integer "rating"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
