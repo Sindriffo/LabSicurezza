ec8c11a1733e108f932a53b6a2ca2c2f42cea446
Alessandro Tozzi <tozzi.1751462@studenti.uniroma1.it>
2020-08-29 11:52:11 +0200

    admin

--- progetto_lassi/db/schema.rb
+++ progetto_lassi/db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200614172717) do
+ActiveRecord::Schema.define(version: 20200829084543) do
+
+  create_table "admins", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
 
   create_table "joinedtravels", force: :cascade do |t|
     t.integer "user_id"
@@ -58,6 +70,7 @@ ActiveRecord::Schema.define(version: 20200614172717) do
     t.string "nome"
     t.string "cognome"
     t.integer "rating"
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
