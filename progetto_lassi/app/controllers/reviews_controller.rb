07bf755174139eb92bcc414a81535971b1935820
donaldhysomemaj <donald.h@hotmail.it>
2020-09-03 17:57:50 +0200

    rev 2.4

--- progetto_lassi/app/controllers/reviews_controller.rb
+++ progetto_lassi/app/controllers/reviews_controller.rb
@@ -1,5 +1,4 @@
 class ReviewsController < ApplicationController
-
     def index
         travel_id = params[:format]
 
@@ -18,5 +17,23 @@ class ReviewsController < ApplicationController
         end	
     end
 
+    def update
+        travel_id = params[:id]
+        # render html: travel_id
+
+        @travel = Travel.find(travel_id)
+        
+        if @travel.user_id == current_user.id
+            @travel.rated = true
+            # @travel.save
+        else
+            @joinedtravel = Joinedtravel.where("joinedtravels.travel_id == ?", travel_id).where("joinedtravels.user_id == ?", current_user.id).first 
+            @joinedtravel.rated = true
+            # @joinedtravel.save
+        end
+
+
+        redirect_to joinedtravels_path
+    end
 
 end
