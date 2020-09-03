5e4611c800a9ca40363047fb29a12b250f509eb1
Donald <donald.h@hotmail.it>
2020-09-03 12:48:24 +0200

    User_stories update

--- progetto_lassi/app/controllers/reviews_controller.rb
+++ progetto_lassi/app/controllers/reviews_controller.rb
@@ -25,25 +25,28 @@ class ReviewsController < ApplicationController
         ####################Update .rated values
         if @travel.user_id == current_user.id
             @travel.rated = true
-            @travel.save
+            # @travel.save
         else
             @joinedtravel = Joinedtravel.where("joinedtravels.travel_id == ?", travel_id).where("joinedtravels.user_id == ?", current_user.id).first 
             @joinedtravel.rated = true
-            @joinedtravel.save
+            # @joinedtravel.save
         end
         
 
         $users.each do |user|
-            rating_value = params[:travel]["rating_" + user.id.to_s].to_i
-            if user.rating == nil
-                user.rating = rating_value
-                user.ratingnum += 1
-            else             
-                temp = user.rating*user.ratingnum + rating_value 
-                user.ratingnum += 1
-                user.rating = temp / user.ratingnum
+            rating_value_string = params[:travel]["rating_" + user.id.to_s]
+            if rating_value_string != " "
+                rating_value = rating_value_string.to_i
+                if user.rating == nil
+                    user.rating = rating_value
+                    user.ratingnum += 1
+                else             
+                    temp = user.rating*user.ratingnum + rating_value 
+                    user.ratingnum += 1
+                    user.rating = temp / user.ratingnum
+                end
             end
-            
+
             segnala_value = params[:travel]["segnala_" + user.id.to_s]
             
             if segnala_value == "si"
