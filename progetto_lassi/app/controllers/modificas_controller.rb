d5bfc1ab204d5085f080011185d69adf12fbdcc9
donaldhysomemaj <donald.h@hotmail.it>
2020-09-05 16:47:11 +0200

    foto

--- progetto_lassi/app/controllers/modificas_controller.rb
+++ progetto_lassi/app/controllers/modificas_controller.rb
@@ -9,6 +9,8 @@ class ModificasController < ApplicationController
 	def create
 		current_user.update("nome" => params[:user][:nome])
 		current_user.update("cognome" => params[:user][:cognome])
+		current_user.update("email" => params[:user][:email])
+		current_user.update("telefono" => params[:user][:telefono])
 		current_user.save
 		redirect_to user_path(current_user)
 	end
